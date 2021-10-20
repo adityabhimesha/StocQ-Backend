7ec0b9e2f1e4c77c3e7cca44a2052a18019d6711
adityabhimesha <aditya.bhimesha456@gmail.com>
2021-10-20 19:24:26 +0530

    DB issue

--- StocQ/settings.py
+++ StocQ/settings.py
@@ -87,23 +87,25 @@ WSGI_APPLICATION = 'StocQ.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if os.environ['ME'] == True:
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': os.environ['DB_NAME'],
-
-        'USER': os.environ['DB_USERNAME'],
-
-        'PASSWORD': os.environ['DB_PASS'],
-
-        'HOST': os.environ['DB_HOST'],
-
-        'PORT': os.environ['DB_PORT'],
-
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USERNAME'],
+            'PASSWORD': os.environ['DB_PASS'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ['DB_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
